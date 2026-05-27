252070c7efd07cd1cce4e8cfdbcc853a0a0dd901
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2026-05-27 20:39:18 +0530

    Loops

---
+++ Q28NaturalNumbers.java
@@ -0,0 +1,16 @@
+// Q28. First 10 Natural Numbers
+
+class Q28NaturalNumbers {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
+
+/*
+Expected Output:
+1 2 3 4 5 6 7 8 9 10
+*/
