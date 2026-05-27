9f3e212b43c45a2d21830f89474ed3dd09b5a877
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2026-05-27 20:52:43 +0530

    Variables_And_Data_Types

---
+++ Q6VariablesDataTypes.java
@@ -0,0 +1,28 @@
+// Q6. Variables and Data Types
+
+class Q6VariablesDataTypes {
+    public static void main(String[] args) {
+
+        int integerValue = 10;
+        float floatValue = 5.75f;
+        double doubleValue = 19.99;
+        char characterValue = 'A';
+        boolean booleanValue = true;
+
+        System.out.println("Integer: " + integerValue);
+        System.out.println("Float: " + floatValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Character: " + characterValue);
+        System.out.println("Boolean: " + booleanValue);
+
+    }
+}
+
+/*
+Expected Output:
+Integer: 10
+Float: 5.75
+Double: 19.99
+Character: A
+Boolean: true
+*/
