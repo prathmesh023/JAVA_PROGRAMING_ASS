5fb9bbde2fbf0b94b38398db423d7fc63a607f49
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2026-05-27 20:46:39 +0530

    Strings

---
+++ Q47Program.java
@@ -0,0 +1,14 @@
+// Q47 Program
+
+class Q47Program {
+    public static void main(String[] args) {
+
+        System.out.println("Program Executed Successfully");
+
+    }
+}
+
+/*
+Expected Output:
+Program Executed Successfully
+*/
