e4fe78bb68cbce4f530557758df327d8692cd74b
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2026-05-27 20:37:10 +0530

    Conditional_Statements

---
+++ Q21EvenOdd.java
@@ -0,0 +1,20 @@
+// Q21. Even or Odd
+
+class Q21EvenOdd {
+    public static void main(String[] args) {
+
+        int number = 7;
+
+        if(number % 2 == 0) {
+            System.out.println(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
+        }
+
+    }
+}
+
+/*
+Expected Output:
+7 is odd
+*/
