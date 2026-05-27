be6460fa8ad9ec5a0145165e42aa18f027337a6a
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2026-05-27 20:45:00 +0530

    Pattern_Matching

---
+++ Q10StarPattern.java
@@ -0,0 +1,16 @@
+// Q10. Star Pattern
+
+class Q10StarPattern {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 15; i++) {
+            System.out.print("* ");
+        }
+
+    }
+}
+
+/*
+Expected Output:
+* * * * * * * * * * * * * * *
+*/
