db3a12064f38416664a44cb0e698c42b8b3745af
Prathmesh Mirage <prathmeshmirage023@gmail.com>
2026-05-28 12:47:41 +0530

    Strings

---
+++ animalname.java
@@ -0,0 +1,16 @@
+//fruit name 
+
+class animalname{
+    public static void main(String args []){
+        String[] animal = new String[3];
+        animal[0]="DOG";
+        animal[1]="LION";
+        animal[2]="TIGER";
+
+        System.out.println("THIS IS A ANIMALS NAME: \n");
+        System.out.println(animal[0]);
+        System.out.println(animal[1]);
+        System.out.println(animal[2]);
+
+    }
+}
